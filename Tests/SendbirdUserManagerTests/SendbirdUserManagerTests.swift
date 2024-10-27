e058062b7ea74d95a7cc605c6c676e1fb259e66d
hassan0424 <hassan@shoplive.cloud>
2024-10-27 13:06:04 +0900

    project init

---
+++ Tests/SendbirdUserManagerTests/SendbirdUserManagerTests.swift
@@ -0,0 +1,27 @@
+//
+//  SendbirdUserManagerTests.swift
+//  SendbirdUserManagerTests
+//
+//  Created by Sendbird
+//
+
+import XCTest
+@testable import SendbirdUserManager
+
+final class UserManagerTests: UserManagerBaseTests {
+    override func userManager() -> SBUserManager {
+        MockUserManager()
+    }
+}
+
+final class UserStorageTests: UserStorageBaseTests {
+    override func userStorage() -> SBUserStorage? {
+        MockUserStorage()
+    }
+}
+
+//final class NetworkClientTests: NetworkClientBaseTests {
+//    override func networkClient() -> SBNetworkClient? {
+//        MockNetworkClient()
+//    }
+//}
